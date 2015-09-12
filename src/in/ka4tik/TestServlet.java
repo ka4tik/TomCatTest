6b28d9752dea09d8da086d5cdbd790b4c3346290
khalu968 <klu1@lion.lmu.edu>
2015-09-13 02:03:32 +0530

    added some simple servlets

---
+++ src/in/ka4tik/TestServlet.java
@@ -0,0 +1,27 @@
+package in.ka4tik;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        String userName = req.getParameter("userName");
+        writer.println("Hello " + userName + " from get!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        String userName = req.getParameter("userName");
+        writer.println("Hello " + userName + " from post!");
+    }
+}
